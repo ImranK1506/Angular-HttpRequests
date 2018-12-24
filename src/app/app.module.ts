5c9e76e7614871866f89354b80d08d4f1c771fc7
Imran Khan <imran.khan@kpn.com>
2018-12-24 14:48:55 +0100

    Youtube Http Requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
-
 import { HttpClientModule } from '@angular/common/http';
+
+import { youtubeSearchInjectables } from './youtube-search/youtube-search.injectables';
+import { YoutubeSearchComponent } from './youtube-search/youtube-search.component';
+import { SearchResultComponent } from './youtube-search/search-result/search-result.component';
+import { SearchBoxComponent } from './youtube-search/search-box/search-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleHttpComponent
+    SimpleHttpComponent,
+    YoutubeSearchComponent,
+    SearchResultComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ youtubeSearchInjectables ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
